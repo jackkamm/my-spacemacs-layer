1d847e29c4adbb230203897ea16f368b35f8527b
Jack Kamm <jackkamm@gmail.com>
2017-11-11 08:05:32 +0000

    set okular as default latex pdf viewer in linux

--- packages.el
+++ packages.el
@@ -30,6 +30,13 @@
   ;; instead of TeX search path
   (setq LaTeX-includegraphics-read-file
         'LaTeX-includegraphics-read-file-relative)
+
+  ;; set okular as default pdf viewer in linux
+  (with-eval-after-load 'tex
+    (when (eq system-type 'gnu/linux)
+            (add-to-list 'TeX-view-program-selection
+                         '(output-pdf "Okular"))
+            ))
   )
 
 ;(defun my-spacemacs-layer/pre-init-evil ()
